0be8d67fe8e55a084ea0990f61fba3bd0bd0413d
MAleemH <aleemhashmi4321@gmail.com>
2023-06-07 10:51:59 +0500

    Added react components to display greeting

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchGreeting } from './redux/greeting/greetingSlice';
+import Greeting from './components/Greeting';
 
-function App() {
-    return (<h1>Hello World!</h1>);
-}
+const App = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchGreeting());
+    }, []);
+
+  return (
+    <Routes>
+        <Route path="/" element={<Greeting />} />
+    </Routes>
+  )
+};
 
 export default App;
